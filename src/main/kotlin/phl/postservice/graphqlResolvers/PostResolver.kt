0dca142b0f733dbea1ddab89f07241e0cae526e0
wyattofgale <thewyattofgale@gmail.com>
2018-11-20 00:25:02 -0600

    trying to get graphql working, no idea why it not

--- src/main/kotlin/phl/postservice/graphqlResolvers/PostResolver.kt
+++ src/main/kotlin/phl/postservice/graphqlResolvers/PostResolver.kt
@@ -2,14 +2,11 @@ package phl.postservice.graphqlResolvers
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
 import org.springframework.stereotype.Component
-import phl.postservice.model.Post
-import java.util.*
 
 
 @Component
 class Query: GraphQLQueryResolver{
 
-    
     fun version() = "0.0.1-SNAPSHOT"
 
 }
